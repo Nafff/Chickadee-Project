6596ccf14c916a746840cbc43df134af958b038b
jadick <adickja@gmail.com>
2021-08-02 16:40:36 -0400

    started basic styling

--- app.js
+++ app.js
@@ -1,33 +1,51 @@
+jungleMonsters = ['tribal-warrior', 'kobold', 'constrictor-snake']
+desertMonsters = ['bandit', 'mummy', 'weretiger-tiger']
+grasslandMonsters = ['blood-hawk', 'panther', 'orc']
+forestMonsters = ['awakened-shrub', 'giant-wolf-spider', 'goblin']
+
+function removeElements(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild);
+  }
+}
+
+// Get value from drop down menu
+document.querySelector('#environmentButton').addEventListener('click', (e) => {
+  e.preventDefault()
+  monsterDiv = document.createElement('div')
+  const selectEnvironment = document.querySelector('#environment').value
+  if (selectEnvironment === 'Desert') {
+    desertMonsters.forEach((monster) => {
+      getMonsters(monster)
+    })
+  } else if (selectEnvironment === 'Jungle') {
+    jungleMonsters.forEach((monster) => {
+      getMonsters(monster)
+    })
+  } else if (selectEnvironment === 'Grassland') {
+    grasslandMonsters.forEach((monster) => {
+      getMonsters(monster)
+    })
+  } else if (selectEnvironment === 'Forest') {
+    forestMonsters.forEach((monster) => {
+      getMonsters(monster)
+    })
+  }
+})
+
+// Run getmonsters on monster list. Append information div inside.
 async function getMonsters(monster) {
+  removeElements(document.querySelector('#monsters'))
   const url = `https://www.dnd5eapi.co/api/monsters/${monster}`
   try {
     const data = await axios.get(url)
-    console.log(data)
-    return data
+    monsterDiv = document.createElement('div')
+    monsterDiv.className = 'monsterCard'
+    monsterDiv.innerText = data.data.name
+    document.querySelector('#monsters').append(monsterDiv)
   } catch (err) {
     console.error(err)
   }
 }
 
-getMonsters('acolyte')
-
-jungleMonsters = ['tribal-warrior', 'kobold', 'constrictor-snake']
-desertMonsters = ['bandit', 'mummy', 'weretiger']
-grasslandMonsters = ['blood-hawk', 'panther', 'orc']
-forestMonsters = ['awakened-shrub', 'giant-wolf-spider', 'goblin']
-
-jungleMonsters.forEach(monster => {
-  getMonsters(monster)
-});
-
-function chooseEnvironment() {
-  document.querySelector('#environmentButton').addEventListener('click', (e) => {
-    e.preventDefault()
-    const selectEnvironment = document.querySelector('#environment').value
-    // const envrionmentType = document.querySelector('#environment').value.textContent
-    document.querySelector('#test').textContent = selectEnvironment
-    console.log(selectEnvironment)
-  })
-}
-
-chooseEnvironment()
+// Make each monster card clickable to pop out information
